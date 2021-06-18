191458d1fea5177ee2b74cf6a755f57df52ff759
Ozkan Akkaya <ozkanakkaya@hotmail.com.tr>
2021-06-18 22:01:27 +0300

    Active sınıfı eklendi(Category menüsüne)

--- shopapp.webui/ViewComponents/CategoriesViewComponent.cs
+++ shopapp.webui/ViewComponents/CategoriesViewComponent.cs
@@ -9,7 +9,8 @@ namespace shopapp.webui.WiewComponents
     {
         public IViewComponentResult Invoke()
         {
-
+            if (RouteData.Values["action"].ToString()=="List")//List sayfasından idyi almak için action List olmalı
+                ViewBag.SelectedCategory=RouteData?.Values["id"];//seçilen menüyü aktif etmek için id yi ViewBag ile tutuyoruz
             return View(CategoryRepository.Categories);
         }
     }
