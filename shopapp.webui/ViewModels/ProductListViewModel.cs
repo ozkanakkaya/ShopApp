79a8892b4fce3810a24589c0ca235e6e9b10f9ee
Ozkan Akkaya <ozkanakkaya@hotmail.com.tr>
2021-06-27 04:01:31 +0300

    Sayfalama link butonlarının hazırlanması

--- shopapp.webui/ViewModels/ProductListViewModel.cs
+++ shopapp.webui/ViewModels/ProductListViewModel.cs
@@ -11,7 +11,7 @@ namespace shopapp.webui.WiewModels
         public int CurrentPage { get; set; }//o anki gösterilen sayfa
         public string CurrentCategory { get; set; }//kategori varsa burada olacak
 
-        int TotalPages()
+        public int TotalPages()
         {
             return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);//Math.Ceiling:yuvarlama iþlemi yapar
         }
