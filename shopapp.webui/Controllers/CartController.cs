dd8f718e2ab80d0ad24480e54a349ea19091888a
Ozkan Akkaya <ozkanakkaya@hotmail.com.tr>
2021-07-06 23:49:49 +0300

    Sepete ekle butonlarının hazırlanması

---
+++ shopapp.webui/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shopapp.webui.Controllers
+{
+    [Authorize]//login olduğunda görünecek
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
