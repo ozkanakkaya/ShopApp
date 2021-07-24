a1b8716a5b531d032c44153b1ffeeb5cb642d770
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2021-07-25 01:10:03 +0300

    RestFull servisin Asp.Net Core projesinde kullanılması

--- shopapp.webui/Controllers/HomeController.cs
+++ shopapp.webui/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using shopapp.business.Abstract;
 using shopapp.data.Abstract;
+using shopapp.entity;
 using shopapp.webui.Models;
 
 namespace shopapp.webui.Controllers
@@ -33,6 +37,22 @@ namespace shopapp.webui.Controllers
         public IActionResult Contact()
         {
             return View("MyView");
+        }
+
+        public async Task<IActionResult> GetProductsFromRestApi()
+        {
+            var products = new List<Product>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response=await httpClient.GetAsync("https://localhost:44371/api/products"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    products = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+                }
+            }
+
+            return View(products);
         }
     }
 }
